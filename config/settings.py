b131bc5d8a359bb43e0557a86baa8aae2f55f88b
yasirnadm143-hue <yasirnadm143@gmail.com>
2026-08-11 19:10:35 +0530

    Configure PostgreSQL for local and Render deployment

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ if DATABASE_URL:
         "default": dj_database_url.parse(
             DATABASE_URL,
             conn_max_age=600,
-            ssl_require=True,
+            ssl_require=False,
         )
     }
 else:
